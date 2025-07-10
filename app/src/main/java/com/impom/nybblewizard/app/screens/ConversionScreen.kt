dbd767a9d28a15a3b744194f733dd49b1b7b0ebf
impom <117834750+impom@users.noreply.github.com>
2025-07-11 01:42:36 +0530

    Cooking with jesse by taking his inputs

--- app/src/main/java/com/impom/nybblewizard/app/screens/ConversionScreen.kt
+++ app/src/main/java/com/impom/nybblewizard/app/screens/ConversionScreen.kt
@@ -1,21 +1,35 @@
 package com.impom.nybblewizard.app.screens
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBackIosNew
 import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.Button
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.impom.nybblewizard.app.reusable.TopNavBar
 
 /**
@@ -49,7 +63,30 @@ fun ConversionScreen(
                 modifier = Modifier.fillMaxSize().padding(innerPadding)
             ){
                 // Start Of Our U.I YESSSS I DID IT
+                var input by remember {mutableStateOf("")}
+            Column(modifier = Modifier
+                .fillMaxSize()
+                .padding(14.dp),
+                verticalArrangement = Arrangement.Center
+            ) {
+                OutlinedTextField(
+                    value = input,
+                    onValueChange = {input = it},
+                    label = { Text("Enter Something Jesse : ")},
+                    modifier = Modifier.fillMaxWidth()
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+
+                Button(onClick = {},
+                    modifier = Modifier.align(Alignment.End)
+                    ) {
+                    Text("Cook")
+                }
+
+            }
+
+            }
         }
         }
     }
-}
+
