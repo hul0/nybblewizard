709a0c50bcb57d515e9e4519822477bbe3b72050
impom <117834750+impom@users.noreply.github.com>
2025-07-11 01:34:04 +0530

    My first UI using Jetpack compose

--- app/src/main/java/com/impom/nybblewizard/app/screens/ConversionScreen.kt
+++ app/src/main/java/com/impom/nybblewizard/app/screens/ConversionScreen.kt
@@ -2,8 +2,21 @@ package com.impom.nybblewizard.app.screens
 
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material.icons.materialIcon
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import com.impom.nybblewizard.app.reusable.TopNavBar
 
 /**
  * A blank Conversion Screen.
@@ -14,17 +27,29 @@ import androidx.compose.ui.Modifier
  * @param onBackClick A lambda function to be invoked when a back navigation action is requested.
  * This can be used to navigate back in your application's navigation graph.
  */
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
+@Preview(showSystemUi = true)
 fun ConversionScreen(
     onBackClick: () -> Unit = {}
 ) {
-    // This Box provides a full-screen container.
-    // You can start building your UI inside this Box.
-    Box(
-        modifier = Modifier.fillMaxSize()
-    ) {
-        // Your Jetpack Compose UI elements will go here.
-        // Example:
-        // Text("Welcome to Conversion Screen!", modifier = Modifier.align(Alignment.Center))
+    MaterialTheme{
+        Scaffold (
+            topBar = { TopAppBar(
+                title = {Text("Conversion")},
+                navigationIcon = {
+                    IconButton(onClick = onBackClick) {
+                        Icon(Icons.Default.ArrowBackIosNew, contentDescription = "Back")
+
+                    }
+                }
+            ) }
+        ){
+            innerPadding -> Box(
+                modifier = Modifier.fillMaxSize().padding(innerPadding)
+            ){
+                // Start Of Our U.I YESSSS I DID IT
+        }
+        }
     }
 }
